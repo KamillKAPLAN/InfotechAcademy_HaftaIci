ecfc50dd67fd7374099aa0a4be4ad5869b3c7dca
KamillKAPLAN <kamilkaplnn@gmail.com>
2023-01-10 19:33:55 +0300

    first commit

---
+++ FirstApp/FirstApp/InfoTech.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace newwww
+{
+    public class InfoTech
+    {
+        public static void yazdir()
+        {
+            Console.WriteLine("Selam arkadaşlar nasılsınız");
+        }
+    }
+}
---
+++ FirstApp/FirstApp/Program.cs
@@ -0,0 +1,29 @@
+﻿using newwww;
+
+namespace FirstApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            string strValue = "Infotech";
+            int intValue = 15;
+            string result = string.Empty;
+
+            Console.WriteLine("Merhaba Sınıf");
+
+            Console.WriteLine("String Value : " + strValue + " İntValue : " + intValue);
+            Console.WriteLine($"String Value : {strValue} İntValue : {intValue}");
+            Console.WriteLine("String Value : {0} İntValue : {1}", strValue, intValue);
+
+            Console.WriteLine(result);
+
+            InfoTech.yazdir();
+
+            /* Ürün Adı : Elma
+             * Ürün Adı : Karpuz
+             * Ürün Adı : Vişne
+             */
+        }
+    }
+}
